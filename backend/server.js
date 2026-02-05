c3c35f2c6d39696587d078a45c8ae78579f3121b
Uma Maheshwar <karnakar231j@gmail.com>
2026-02-05 13:27:45 +0530

    Set up Express server with CORS and Sequelize

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
 
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const sequelize = require("./config/db");
+const jobRoutes = require("./routes/jobRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", jobRoutes);
+
+sequelize.authenticate()
+  .then(() => console.log("DB Connected"))
+  .catch(err => console.log(err));
+
+app.listen(process.env.PORT, () => {
+  console.log("Server running on port", process.env.PORT);
+});
